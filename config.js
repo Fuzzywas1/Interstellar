6da34c3375323246adea6e39eeceb746b836d346
Fuzzywas1 <Dvestal20211@gmail.com>
2025-11-15 15:27:07 -0600

    Enable password protection and update user password

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    fuzzthehuzz: "1234",
-    andy: "1234"
+    fuzzthehuzz: "fuzzisthehuzz",
   },
 };
 
